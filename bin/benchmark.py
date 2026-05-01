9f5116b139af219068a633599f677c9553a6f5b0
htjb <harrybevins@proton.me>
2026-05-01 17:55:31 +0100

    fixing skip-gpu bug

--- bin/benchmark.py
+++ bin/benchmark.py
@@ -18,6 +18,7 @@ from hyprfine.analytic.main import generate_signal
 from hyprfine.parameters import cosmology, astrophysics
 import sys
 
+skip_gpu = False
 if len(sys.argv) > 1 and sys.argv[1] == "--no-gpu":
     skip_gpu = True
 
